8f358e73cd6811b60d4bbe7c0315c30a2b94e50d
Cristyan Alves de Oliveira Almeida. <alvescristyan606@gmail.com>
2025-01-04 16:17:50 -0300

    Atividade questão 17

---
+++ Atividade_Q3.java
@@ -0,0 +1,43 @@
+/*
+ VAR
+real:celsius,fahrenheit
+cadeia
+
+INICIO
+leia
+ (temperaura em calsius:...)
+
+temperatura em Fº
+fahrenheit = (celsius*1.8)+32
+
+escreva o valor convertido...
+
+fim
+
+TESTE DE MESA
+
+CELSIUS / FAHRENHEIT / IMPRIMA /
+ 25          77         77
+-2          28,4        28,4
+
+ */
+
+package com.mycompany.atividade_q3;
+import javax.swing.JOptionPane;
+
+public class Atividade_Q3 {
+
+    public static void main(String[] args) {
+        double celsius,fahrenheit;
+        String s_celsius;
+        
+        s_celsius = JOptionPane.showInputDialog(null, "Digite a Temperatura Atual: \n - Atual (Celsius) \n - Para (Fahrenheit)");
+        celsius = Double.parseDouble(s_celsius);
+        
+        fahrenheit = (celsius*1.8)+32;
+        
+        JOptionPane.showMessageDialog(null,String.format("A temperatura convertida será %.1f ",+ fahrenheit));
+                
+    }
+    
+}
